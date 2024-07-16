adf10815b38a6699d91803aa2ad46789d85904d0
alejuandroo <gratner@live.se>
2024-07-17 01:00:13 +0200

    Update fish prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,10 @@ if status is-interactive
     set -gx EDITOR nvim
 end
 
+
+function fish_prompt -d "Write out the prompt"
+    printf '%s' (set_color yellow) $USER ' ' \
+      (set_color brwhite) $PWD (set_color brblue    ) (fish_git_prompt) \
+      (set_color normal ) \n'> '
+end
+
