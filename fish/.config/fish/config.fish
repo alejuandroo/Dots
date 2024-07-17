a7b078af1b3c07d43f3744934b096720b53b8c54
alejuandroo <gratner@live.se>
2024-07-17 12:43:03 +0200

    Add fish abbrevations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,5 +13,10 @@ end
 alias vim=nvim
 abbr --add v vim
 abbr --add gs git status
+abbr --add gsu git status -uno
 abbr --add gd git diff
+abbr --add gds git diff --staged
 abbr --add gdd "git diff | delta --side-by-side"
+
+# NOTE: Location specific abbrevations
+abbr --add d cd ~/dotfiles
